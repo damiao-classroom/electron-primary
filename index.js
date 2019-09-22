991ea3520a287417f9eaf6ae2a1041e1f8b6ebdb
damiao-classroom <lgc928091232@163.com>
2019-09-22 19:00:18 +0800

    文件菜单和开发者菜单

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const electron = require('electron')
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 const path = require('path');
 const url = require('url');
 
@@ -13,4 +13,46 @@ app.on('ready', () => {
         protocol: 'file:',
         slashes: true
     }));
+
+    // 定义菜单
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 });
+
+// 顶部菜单 模板
+const menuTemplate = [
+    // 文件菜单项
+    {
+        // 顶部菜单的名称
+        label: '文件',
+        // 顶部菜单下面的子菜单列表
+        submenu: [
+            { label: '新增信息' },
+            { label: '清空信息' },
+            { label: '退出', 
+            accelerator: process.platform == 'darwin' ? 'Command+D' : 'Ctrl+D',
+            click: () => {
+                // 退出
+                app.quit();
+            } 
+            }
+        ]
+    },
+    // 开发者工具菜单项
+    {
+        label: '开发者工具',
+        submenu: [
+            { 
+                label: '打开/关闭',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I', 
+                click: (item, focusedWindow) => {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            { label: '刷新一下', 
+              role: 'reload', 
+              accelerator: process.platform == 'darwin' ? 'Command+F5' : 'Ctrl+F5' 
+            }
+        ]
+    }
+]
