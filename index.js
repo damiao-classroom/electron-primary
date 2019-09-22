0eb92e40d57d4b9457c0ddba877e7a77e1e972d5
damiao-classroom <lgc928091232@163.com>
2019-09-22 16:56:40 +0800

    new

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const electron = require('electron')
 const { app, BrowserWindow } = electron;
+const path = require('path');
+const url = require('url');
 
 let win = null;
 app.on('ready', () => {
     win = new BrowserWindow({
         width: 800, height: 600
     });
-    win.loadURL("https://www.baidu.com")
-})
+    win.loadURL(url.format({
+        pathname: path.resolve(__dirname,'./html/main.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+});
